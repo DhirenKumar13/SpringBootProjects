ed5e270ef2a8375dcc5e074717b467b637e4a840
dhiren13_11 <dhirenwipro@gmail.com>
2018-04-12 23:48:34 +0530

    Spring Boot With RabbitMQ

---
+++ springboot-starter-api/src/main/java/io/dhirenjava/springboot/springbootstarterapi/SpringbootStarterApiApplication.java
@@ -0,0 +1,12 @@
+package io.dhirenjava.springboot.springbootstarterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStarterApiApplication.class, args);
+	}
+}
---
+++ springboot-starter-api/src/test/java/io/dhirenjava/springboot/springbootstarterapi/SpringbootStarterApiApplicationTests.java
@@ -0,0 +1,16 @@
+package io.dhirenjava.springboot.springbootstarterapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootStarterApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
